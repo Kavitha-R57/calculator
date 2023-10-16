090e1039fdb1d58dfa67d2c61ead0a1c8f8b8d58
Kavitha-R57 <srkavi57@gmail.com>
2023-10-16 10:35:31 +0530

    modify

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,13 +1,36 @@
-// src/App.js
-import React from 'react';
-import './App.css';
-import Calculator from './Calculator';
-
+import "./App.css";
+import React, {useState} from 'react'
 function App() {
+const [value,setValue]=useState("");
+
+
+  const buttons =["AC",".","/","7","8","9","*","4","5","6","+","1","2","3","-","00","0","=","%"];
+  function handleButtons(keys){
+    if(keys==="AC"){
+      setValue("");
+   }else if(keys==="DEL"){
+    setValue(value.slice(0,-1));
+   }
+   else{
+    setValue(value+keys)
+   }
+  
+  }
+
+
+
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    
+        <div className="calculator">
+          <input type="text" value={value}></input>
+          {buttons.map((buttons,index)=>(
+           <button value={buttons} key={index} onClick={()=>handleButtons(buttons)}>{buttons}</button>
+          
+          ))}
+            
+          </div>
+     
+
   );
 }
 
--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -66,36 +66,31 @@ class Calculator extends Component {
       case '+':
         this.setState({
           displayValue: String(parseFloat(firstOperand) + parseFloat(secondOperand)),
-          operator: null,
-          firstOperand: null,
+          
         });
         break;
       case '-':
         this.setState({
           displayValue: String(parseFloat(firstOperand) - parseFloat(secondOperand)),
-          operator: null,
-          firstOperand: null,
+          
         });
         break;
       case '*':
         this.setState({
           displayValue: String(parseFloat(firstOperand) * parseFloat(secondOperand)),
-          operator: null,
-          firstOperand: null,
+         
         });
         break;
       case '/':
         if (parseFloat(secondOperand) === 0) {
           this.setState({
             displayValue: 'Error',
-            operator: null,
-            firstOperand: null,
+           
           });
         } else {
           this.setState({
             displayValue: String(parseFloat(firstOperand) / parseFloat(secondOperand)),
-            operator: null,
-            firstOperand: null,
+          
           });
         }
         break;
@@ -107,35 +102,35 @@ class Calculator extends Component {
   render() {
     const { displayValue } = this.state;
 
-    return (
-      <div className="calculator">
-        <div className="display">{displayValue}</div>
-        <div className="keypad">
-          <div className="buttons">
-            <button onClick={() => this.inputDigit(7)}>7</button>
-            <button onClick={() => this.inputDigit(8)}>8</button>
-            <button onClick={() => this.inputDigit(9)}>9</button>
-            <button onClick={() => this.inputDigit(4)}>4</button>
-            <button onClick={() => this.inputDigit(5)}>5</button>
-            <button onClick={() => this.inputDigit(6)}>6</button>
-            <button onClick={() => this.inputDigit(1)}>1</button>
-            <button onClick={() => this.inputDigit(2)}>2</button>
-            <button onClick={() => this.inputDigit(3)}>3</button>
-            <button onClick={this.clearInput}>C</button>
-            <button onClick={() => this.inputDigit(0)}>0</button>
-            <button onClick={this.inputDecimal}>.</button>
-          </div>
-          <div className="operators">
-            <button onClick={() => this.setOperator('+')}>+</button>
-            <button onClick={() => this.setOperator('-')}>-</button>
-            <button onClick={() => this.setOperator('*')}>*</button>
-            <button onClick={() => this.setOperator('/')}>/</button>
-            <button onClick={this.calculate}>=</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+    // return (
+      // <div className="calculator">
+      //   <div className="display">{displayValue}</div>
+      //   <div className="keypad">
+      //     <div className="buttons">
+      //       <button onClick={() => this.inputDigit(7)}>7</button>
+      //       <button onClick={() => this.inputDigit(8)}>8</button>
+      //       <button onClick={() => this.inputDigit(9)}>9</button>
+      //       <button onClick={() => this.inputDigit(4)}>4</button>
+      //       <button onClick={() => this.inputDigit(5)}>5</button>
+      //       <button onClick={() => this.inputDigit(6)}>6</button>
+      //       <button onClick={() => this.inputDigit(1)}>1</button>
+      //       <button onClick={() => this.inputDigit(2)}>2</button>
+      //       <button onClick={() => this.inputDigit(3)}>3</button>
+      //       <button onClick={this.clearInput}>C</button>
+      //       <button onClick={() => this.inputDigit(0)}>0</button>
+      //       <button onClick={this.inputDecimal}>.</button>
+      //     </div>
+      //     <div className="operators">
+      //       <button onClick={() => this.setOperator('+')}>+</button>
+      //       <button onClick={() => this.setOperator('-')}>-</button>
+      //       <button onClick={() => this.setOperator('*')}>*</button>
+      //       <button onClick={() => this.setOperator('/')}>/</button>
+      //       <button onClick={this.calculate}>=</button>
+      //     </div>
+      //   </div>
+    //   </div>
+    // );
+  // }
+// }
 
 export default Calculator;
